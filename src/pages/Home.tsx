b7222b519a433263708ef925d280bf3ac000dc7e
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-09-07 19:59:09 -0300

    feat: setup da estrutura geral

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+export default function Home()
+{
+    return (
+        <div>
+            <h1>Página Inicial </h1>
+            
+            <p>
+                Arcgardennnnnn
+            </p>
+        </div>
+    );
+}
