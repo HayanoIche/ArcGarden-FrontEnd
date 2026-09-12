7fb4c6df0095ee7da8a4b4450ef62a34a14c092e
Matheus Pereira Vidal <matiaswilson0717@gmail.com>
2026-09-12 15:26:10 -0300

    feat: rota da página de contato adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LayoutBase from './layouts/LayoutBase.tsx';
 
 import Home from './pages/Home';
+import Contato from './pages/Contato';
 
 export default function App()
 {
@@ -10,6 +11,7 @@ export default function App()
       <Routes>
         <Route element={<LayoutBase />}>
           <Route path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
         </Route>
       </Routes>
     </BrowserRouter>
