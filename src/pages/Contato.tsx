79ecad28e7ea47d3f4954f531ca860907d95f3b3
Matheus Pereira Vidal <matiaswilson0717@gmail.com>
2026-09-12 15:18:09 -0300

    feat: formulário de contato criado

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,12 +1,59 @@
 function Contato() {
   return (
     <main>
-      <h1>Fale conosco</h1>
+      <section>
+        <div>
+          <h2>
+            Fale <em>conosco</em>
+          </h2>
 
-      <p>
-        Tem alguma dúvida, sugestão ou quer saber mais sobre o ArcGarden?
-        Entre em contato com a gente!
-      </p>
+          <p>
+            Envie suas dúvidas, sugestões ou feedback sobre o Arcgarden!
+          </p>
+
+          <form>
+            <div>
+              <label>Nome completo</label>
+              <input
+                type="text"
+                placeholder="Seu nome"
+                required
+              />
+            </div>
+
+            <div>
+              <label>E-mail</label>
+              <input
+                type="email"
+                placeholder="seu@email.com"
+                required
+              />
+            </div>
+
+            <div>
+              <label>Assunto</label>
+              <select required>
+                <option value="">Selecione</option>
+                <option>Dúvida</option>
+                <option>Sugestão</option>
+                <option>Problema</option>
+              </select>
+            </div>
+
+            <div>
+              <label>Mensagem</label>
+              <textarea
+                placeholder="Escreva sua mensagem"
+                required
+              />
+            </div>
+
+            <button type="submit">
+              Enviar mensagem
+            </button>
+          </form>
+        </div>
+      </section>
     </main>
   );
 }
