1aca5adff0f0778f80060d722b05f9a81b585748
Matheus Pereira Vidal <matiaswilson0717@gmail.com>
2026-09-12 14:39:02 -0300

    feat: estrutura inicial da página de contato

---
+++ src/pages/Contato.tsx
@@ -0,0 +1,14 @@
+function Contato() {
+  return (
+    <main>
+      <h1>Fale conosco</h1>
+
+      <p>
+        Tem alguma dúvida, sugestão ou quer saber mais sobre o ArcGarden?
+        Entre em contato com a gente!
+      </p>
+    </main>
+  );
+}
+
+export default Contato;
