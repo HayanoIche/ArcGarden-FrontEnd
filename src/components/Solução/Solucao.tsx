605801a36c5dbf5e1bb68b45874d417fcf845a43
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-09-13 00:02:27 -0300

    change: divs pra sections

--- src/components/Solução/Solucao.tsx
+++ src/components/Solução/Solucao.tsx
@@ -3,23 +3,37 @@ export default function Solucao()
 {
     return (
         <div>
-            <div>
-                <span>Conheça o projeto</span>
-                <h2>Como é o <em>Arcgarden</em></h2>
-                <p>UI do aplicativo</p>
-            </div>
+            <section>
+                <span>
+                    Conheça o projeto
+                </span>
+                
+                <h2>
+                    Como é o <em> Arcgarden </em>
+                </h2>
+                
+                <p>
+                    UI do aplicativo
+                </p>
+            </section>
 
-            <div>
+            <section>
                 <div>
-                    <h2> exemplo da UI do app </h2>
+                    <h2>
+                        exemplo da UI do app
+                    </h2>
+                    
                     <img src="../images/app/ui.png" alt="" id="ui" />
                 </div>
 
                 <div>
-                    <h2> Fluxo da gameplay </h2>
+                    <h2>
+                        Fluxo da gameplay
+                    </h2>
+                    
                     <img src="../images/app/fluxo.png" alt="" id="fluxo"/>
                 </div>
-            </div>
+            </section>
         </div>
     );
 }
