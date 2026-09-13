ce314e2ca9e3f319d82277243e8359e014f220b3
wonbindasilva <bloomhw4@gmail.com>
2026-09-13 14:19:51 -0300

    feat: secao de funcionalidades na home

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -8,16 +8,16 @@ export default function HeroSection() {
       />
 
       <div className="flex flex-col items-center">
-        <h2 className="flex flex-col items-center text-[var(--g4)] text-[3rem] lg:text-[5rem] leading-tight">
+        <h2 className="flex flex-col items-center font-bold text-[var(--g4)] text-[3rem] lg:text-[5rem] leading-tight">
           Seu jardim virtual,{' '}
           <em
   style={{ fontFamily: 'var(--font-spectral)' }}
-  className="not-italic text-[var(--g3)]"
+  className="italic text-[var(--g3)]"
 >
   impacto real
 </em>
         </h2>
-        <p className="text-[var(--text-main)] pt-4 lg:pt-12 text-[1.2rem] lg:text-[1.75rem] max-w-[40rem]">
+        <p className="font-bold text-[var(--text-main)] pt-4 lg:pt-12 text-[1.2rem] lg:text-[1.75rem] max-w-[40rem]">
           Faça missões sustentáveis no dia a dia, cuide do seu jardim virtual
           e concorra a ter sua conta de energia paga.
         </p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-
-
 import HeroSection from '../components/Home/HeroSection';
+import FuncionalidadesSection from '../components/Home/Funcionalidades';
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <FuncionalidadesSection />
     </div>
   );
 }
