f2b7d308206b6edd7e23907fb152283f7210ccc6
wonbindasilva <bloomhw4@gmail.com>
2026-09-13 19:46:51 -0300

    fix: organizacao texto da parceria

--- src/components/Sobre/Parceria.tsx
+++ src/components/Sobre/Parceria.tsx
@@ -1,7 +1,8 @@
 export default function Parceria() {
   return (
-    <div className="w-full max-w-5xl bg-[var(--g1)] border-[0.2rem] border-[var(--g3)] rounded-3xl p-8 mt-12">
-      <h3 className="font-bold text-[var(--text-bold)] text-xl mb-4">
+    <div className="w-full max-w-5xl bg-[var(--g1)] border-[0.2rem] border-[var(--g3)] rounded-3xl p-6 lg:p-8 mt-12">
+      
+      <h3 className="font-bold text-[var(--text-bold)] text-xl mb-5">
         Parceria com a{' '}
         <em
           style={{ fontFamily: 'var(--font-spectral)' }}
@@ -10,7 +11,8 @@ export default function Parceria() {
           Soul Up
         </em>
       </h3>
-      <p className="font-bold text-[var(--text-main)]">
+
+      <p className="font-bold text-[var(--text-main)] leading-relaxed">
         A Soul Up é um ecossistema digital focado em sustentabilidade, no qual
         o Arcgarden atua como uma funcionalidade de gamificação integrada.
         Nele, os pontos conquistados ao longo do jogo podem ser convertidos em
@@ -18,6 +20,7 @@ export default function Parceria() {
         do Vale Energia. A cada missão completada, o usuário contribui com os
         Objetivos de Desenvolvimento Sustentável (ODS) da ONU.
       </p>
+
     </div>
   );
 }
