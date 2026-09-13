f1e3a93808418e06dcb5cebe973af730aa330526
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-09-12 23:55:35 -0300

    feat: HTML importado para o componente da solução

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LayoutBase from './layouts/LayoutBase.tsx';
 
 import Home from './pages/Home';
+import Solucao from './components/Solução/Solucao.tsx';
 
 export default function App()
 {
@@ -10,6 +11,7 @@ export default function App()
       <Routes>
         <Route element={<LayoutBase />}>
           <Route path="/" element={<Home />} />
+          <Route path="/solucao" element={<Solucao />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/Solução/Solucao.tsx
@@ -0,0 +1,25 @@
+
+export default function Solucao()
+{
+    return (
+        <div>
+            <div>
+                <span>Conheça o projeto</span>
+                <h2>Como é o <em>Arcgarden</em></h2>
+                <p>UI do aplicativo</p>
+            </div>
+
+            <div>
+                <div>
+                    <h2> exemplo da UI do app </h2>
+                    <img src="../images/app/ui.png" alt="" id="ui" />
+                </div>
+
+                <div>
+                    <h2> Fluxo da gameplay </h2>
+                    <img src="../images/app/fluxo.png" alt="" id="fluxo"/>
+                </div>
+            </div>
+        </div>
+    );
+}
