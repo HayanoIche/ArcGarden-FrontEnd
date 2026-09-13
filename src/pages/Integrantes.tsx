13dc3ab6b1cc5810fc5f0a73e9eef904f478669e
Franco <francobr2025@gmail.com>
2026-09-13 18:38:31 -0300

    feat: importando a pagina de integrantes antiga

---
+++ src/pages/Integrantes.tsx
@@ -0,0 +1,65 @@
+
+export default function Integrantes()
+{
+    return (
+        <div>
+            <h2>Nossa equipe</h2>
+            <p>Conheça os integrantes do Arcgarden</p>
+
+            <div>
+
+            <div>
+            <img src="../images/integrantes/danielle.jpeg" alt="Foto do integrante" width="150"/>
+
+            <h3>Franco Jared Marquina Blas</h3>
+            <p><strong>RM:</strong> 572901</p>
+            <p><strong>Turma:</strong> 1TDSPK</p>
+
+            <p>
+                <a href="https://www.linkedin.com/in/franco-jared-marquina-blas-b67851303/" target="_blank">LinkedIn</a> |
+                <a href="https://github.com/Fmarquina" target="_blank">GitHub</a>
+            </p>
+        </div>
+
+        <div >
+            <img src="../images/integrantes/igor.jpeg" alt="Foto do integrante" width="150"/>
+
+            <h3>Igor Mateus da Silva</h3>
+            <p><strong>RM:</strong> 571751</p>
+            <p><strong>Turma:</strong> 1TDSPK</p>
+
+            <p>
+                <a href="https://www.linkedin.com/in/igor-mateus-da-silva-4b05013ba/" target="_blank">LinkedIn</a> |
+                <a href="https://github.com/HayanoIche" target="_blank">GitHub</a>
+            </p>
+        </div>
+
+        <div>
+            <img src="../images/integrantes/marcela.jpeg" alt="Foto do integrante" width="150"/>
+
+            <h3>Marcela Batista Teixeira</h3>
+            <p><strong>RM:</strong> 574069</p>
+            <p><strong>Turma:</strong> 1TDSPK</p>
+
+            <p>
+                <a href="https://www.linkedin.com/in/marcelabteixeira/" target="_blank">LinkedIn</a> |
+                <a href="https://github.com/wonbindasilva" target="_blank">GitHub</a>
+            </p>
+        </div>
+
+        <div >
+            <img src="../images/integrantes/matheus.jpeg" alt="Foto do integrante" width="150"/>
+
+            <h3>Matheus Pereira Vidal</h3>
+            <p><strong>RM:</strong> 573327</p>
+            <p><strong>Turma:</strong> 1TDSPK</p>
+            
+            <p>
+                <a href="https://linkedin.com/in/matheus-pereira-8700893b3" target="_blank">LinkedIn</a> |
+                <a href="https://github.com/pereirinh44" target="_blank">GitHub</a>
+            </p>
+        </div>
+    </div>
+    </div>
+    );
+}
